2f67dad3659d8a919890d4564ab72ae010286f44
Theekush <kuriacharles560@gmail.com>
2022-09-12 09:26:16 -0700

    Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,13 @@
+/*
+ *
+ *  * File: 5-printf.c
+ *
+ *   * Auth: Brennan D Baraban
+ *
+ *    */
+
+
+
 #include <stdio.h>
 
 
@@ -25,3 +35,15 @@ int main(void)
 			return (0);
 
 }
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
+
+Securi
